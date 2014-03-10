8546ce646b213667a6db987cefc2d06113a46125
Andrew Kessler <andrewkessler6@gmail.com>
2014-03-10 16:49:11 -0700

    Moved functions out of main.cpp

--- Code/SailingCoach/SailingCoach/ColorSegmentation.h
+++ Code/SailingCoach/SailingCoach/ColorSegmentation.h
@@ -10,6 +10,8 @@
 #define __SailingCoach__ColorSegmentation__
 
 #include <iostream>
+#include "opencv2/opencv.hpp"
+using namespace cv;
 
 //This is a struct type that will hold min/max in HSV space
 typedef struct
@@ -25,7 +27,16 @@ typedef struct
 
 void readSettingsFromFile(Threshold_t &, std::string);
 void saveSettingsToFile(Threshold_t &, std::string);
-void createTrackbars(Threshold_t&, const std::string);
 
+
+void drawObject(int, int, Mat &, Scalar);
+void trackFilteredObject(Mat threshold, Mat &cameraFeed, Point2d&);
+void runColorSegmentation(VideoCapture cap, double dWidth, double dHeight);
+void createTrackbars(Threshold_t&, const string);
+void morphOps(Mat &);
+void drawCenterAxes(Mat&,Size,Scalar);
+
+#define  GREEN  Scalar(0,255,0)
+#define  BLUE   Scalar(255,0,0)
 
 #endif /* defined(__SailingCoach__SettingsLibrary__) */
